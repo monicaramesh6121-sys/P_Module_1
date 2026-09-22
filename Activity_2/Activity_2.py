e555e6b5f76c0e71a3dc853ed8876c8a86e9022e
monica-deswal <monica.hardwaria@htsol.in>
2026-09-22 17:55:48 +0530

    initial commit

---
+++ Activity_2/Activity_2.py
@@ -0,0 +1,28 @@
+a = 5
+
+print(type(a))
+b = 2.5
+print(type(b))
+c = "coding"
+print(type(c))
+d = True
+print(type(d))
+
+#Typecasting
+f = 10.18
+i = int(f)
+print(i)
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+#String Indexing and Slicing
+word = "mango"
+print(word[0])
+print(word[1:4])
+print(word[-1])
+
+#String Concatenation
+fruit1 = "apple"
+fruit2 = "banana"
+print(fruit1 + " and " + fruit2)
